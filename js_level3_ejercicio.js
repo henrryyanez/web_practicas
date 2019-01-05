82973afa88b335cf77510af38b0eea03ade94534
hy <henrryyanez@gmail.com>
2019-01-05 18:28:52 -0300

    Probando con un conversor de Libras a Kilos

---
+++ js_level3_ejercicio.js
@@ -0,0 +1,6 @@
+alert("Bienvenido al conversor de Libras a kilograms");
+var pLibra = prompt("Ingrese el valor en Libras para convertir a kilograms: ")
+alert("El varor ingresado es: "+pLibra)
+var pKilo = pLibra * 0.454
+alert("La conversión arroja que en Kilos equivale a: "+pKilo)
+console.log("Conversión completada");
