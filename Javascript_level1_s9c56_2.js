221e13187182d67cfc0ae653375441c7311ec36f
hy <henrryyanez@gmail.com>
2019-01-05 22:29:22 -0300

    Create Javascript_level1_s9c56_2.js

---
+++ Javascript_level1_s9c56_2.js
@@ -0,0 +1,13 @@
+var x = 0;
+
+while (x < 5) {
+  console.log("x is currently: "+x)
+
+  if (x===3) {
+    console.log("X IS EQUAL TO THEER!")
+  }
+
+  console.log("x is still less then 5, adding 1 to x");
+
+  x = x+1;
+}
