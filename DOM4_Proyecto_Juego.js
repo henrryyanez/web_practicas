35f7901e219286d881313b51d73440b8fd901958
hy <henrryyanez@gmail.com>
2019-01-12 20:28:37 -0300

    Javascript con interacción, falta poder reiniciar

---
+++ DOM4_Proyecto_Juego.js
@@ -0,0 +1,118 @@
+var rowOne = document.querySelector('#r1')
+var rowTwo = document.querySelector('#r2')
+var rowThree = document.querySelector('#r3')
+var rowFour = document.querySelector('#r4')
+var rowFive = document.querySelector('#r5')
+var rowSix = document.querySelector('#r6')
+var rowSeven = document.querySelector('#r7')
+var rowEight = document.querySelector('#r8')
+var rowNine = document.querySelector('#r9')
+
+
+// Row One
+
+rowOne.addEventListener('click', function(){
+  rowOne.textContent = 'X';
+  rowOne.style.color = 'red';
+})
+
+rowOne.addEventListener('dblclick', function(){
+  rowOne.textContent = 'O';
+  rowOne.style.color = 'blue';
+})
+
+// Row Two
+
+rowTwo.addEventListener('click', function(){
+  rowTwo.textContent = 'X';
+  rowTwo.style.color = 'red';
+})
+
+rowTwo.addEventListener('dblclick', function(){
+  rowTwo.textContent = 'O';
+  rowTwo.style.color = 'blue';
+})
+
+// Row three
+
+rowThree.addEventListener('click', function(){
+  rowThree.textContent = 'X';
+  rowThree.style.color = 'red';
+})
+
+rowThree.addEventListener('dblclick', function(){
+  rowThree.textContent = 'O';
+  rowThree.style.color = 'blue';
+})
+
+// Row Four
+
+rowFour.addEventListener('click', function(){
+  rowFour.textContent = 'X';
+  rowFour.style.color = 'red';
+})
+
+rowFour.addEventListener('dblclick', function(){
+  rowFour.textContent = 'O';
+  rowFour.style.color = 'blue';
+})
+
+// Row Five
+
+rowFive.addEventListener('click', function(){
+  rowFive.textContent = 'X';
+  rowFive.style.color = 'red';
+})
+
+rowFive.addEventListener('dblclick', function(){
+  rowFive.textContent = 'O';
+  rowFive.style.color = 'blue';
+})
+
+// Row Six
+
+rowSix.addEventListener('click', function(){
+  rowSix.textContent = 'X';
+  rowSix.style.color = 'red';
+})
+
+rowSix.addEventListener('dblclick', function(){
+  rowSix.textContent = 'O';
+  rowSix.style.color = 'blue';
+})
+
+// Row Seven
+
+rowSeven.addEventListener('click', function(){
+  rowSeven.textContent = 'X';
+  rowSeven.style.color = 'red';
+})
+
+rowSeven.addEventListener('dblclick', function(){
+  rowSeven.textContent = 'O';
+  rowSeven.style.color = 'blue';
+})
+
+// Row Eight
+
+rowEight.addEventListener('click', function(){
+  rowEight.textContent = 'X';
+  rowEight.style.color = 'red';
+})
+
+rowEight.addEventListener('dblclick', function(){
+  rowEight.textContent = 'O';
+  rowEight.style.color = 'blue';
+})
+
+// Row Nine
+
+rowNine.addEventListener('click', function(){
+  rowNine.textContent = 'X';
+  rowNine.style.color = 'red';
+})
+
+rowNine.addEventListener('dblclick', function(){
+  rowNine.textContent = 'O';
+  rowNine.style.color = 'blue';
+})
