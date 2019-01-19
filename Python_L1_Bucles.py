34935bb234c43869b089961cd20e933075306787
hy <henrryyanez@gmail.com>
2019-01-19 17:59:06 -0300

    Create Python_L1_Bucles.py

---
+++ Python_L1_Bucles.py
@@ -0,0 +1,16 @@
+# For Loops
+
+seq = [1,2,3,4,5,6]
+
+for henrry in seq:
+    # Code here
+    print(henrry)
+     
+
+# For Loops
+
+seq = [1,2,3,4,5,6]
+
+for henrry in seq:
+    # Code here
+    print('HOLA')
