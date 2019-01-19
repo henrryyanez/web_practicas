9d5884ea8382bea53d1f64963f30768bdb2f222f
hy <henrryyanez@gmail.com>
2019-01-19 16:48:43 -0300

    Funcionando

--- Python_L1_Ejercicio1.py
+++ Python_L1_Ejercicio1.py
@@ -24,7 +24,7 @@ print(s[1:4])
 # 'go'
 print(s[4:6])
 # Bonus: Use indexing to reverse the string
-
+s[::-1]
 
 ###############
 ## Problem 2 ##
@@ -34,8 +34,8 @@ print(s[4:6])
 l = [3,7,[1,4,'hello']]
 print(l)
 # Reassign "hello" to be "goodbye"
-# l[2] = 'goodbye'
-# print(l)
+l[2][2] = 'goodbye'
+print(l)
 
 ###############
 ## Problem 3 ##
@@ -43,12 +43,16 @@ print(l)
 
 # Using keys and indexing, grab the 'hello' from the following dictionaries:
 
-d1 = {'simple_key':'hello'}
-
-d2 = {'k1':{'k2':'hello'}}
-
-d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
-
+# d1 = {'simple_key':'hello'}
+#
+# d1['simple_key']
+#
+# d2 = {'k1':{'k2':'hello'}}
+#
+# d['k1']['k2']
+#
+# d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+# d3['k1'][0]['nest_key'][1][0]
 
 ###############
 ## Problem 4 ##
@@ -70,4 +74,4 @@ name = "Sammy"
 
 # Use print formatting to print the following string:
 # "Hello my dog's name is Sammy and he is 4 years old"
-print("Hello my dog's name is "+ "name " + 'and he is ' + 'age ' + 'years ord"')
+print("Hello my dog's name is {a} and he is {b} years old.".format(a=age,b=name))
