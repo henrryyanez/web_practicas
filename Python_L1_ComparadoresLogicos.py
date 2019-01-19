7621aab1128e3d9daf601b49c06681261c212870
hy <henrryyanez@gmail.com>
2019-01-19 17:14:53 -0300

    Inicial de comparadores logicos

---
+++ Python_L1_ComparadoresLogicos.py
@@ -0,0 +1,18 @@
+#############################
+#### COMPARADORES LOGICOS####
+#############################
+
+# Greater Than
+1>2
+# Less Than
+1<2
+# Greater than or equal to
+1>=1
+# Less than or Equal to
+1<=4
+# Equality
+1 === 1
+1 == "1"
+'hi' == 'bye'
+# Inequality
+1 != 2
