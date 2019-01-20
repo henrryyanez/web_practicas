9069c7f697f79005a2e2ceb0ff2071e6249287e1
hy <henrryyanez@gmail.com>
2019-01-20 01:02:15 -0300

    Comprobador de Enteros y String

--- Python_L1_funciones.py
+++ Python_L1_funciones.py
@@ -4,3 +4,14 @@ def hello():
 result = hello()
 
 print(result)
+
+
+
+def addNum(num1,num2):
+    if type(num1)==type(num2)==type(10):
+        return num1+num2
+    else:
+        return "Sorry, I need integers!"
+
+result = addNum("2",'3')
+print(result)
