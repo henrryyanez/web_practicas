470248a54916c0135d8c968310cc7a03fab93ecf
hy <henrryyanez@gmail.com>
2019-01-20 21:40:37 -0300

    Lambda Explression Filter

--- Python_L1_funciones.py
+++ Python_L1_funciones.py
@@ -15,3 +15,16 @@ def addNum(num1,num2):
 
 result = addNum("2",'3')
 print(result)
+
+
+
+# Lambda Explression
+
+# Filter
+mylist = [1,2,3,4,5,6,7,8]
+
+def even_bool(num):
+    return num%2 == 0
+
+evens = filter(even_bool,mylist)
+print(list(evens))
