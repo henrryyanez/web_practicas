74719b53d2b251e0f6d9c7191cabf161c7002884
hy <henrryyanez@gmail.com>
2019-01-20 22:04:04 -0300

    Update Python_L1_funciones.py

--- Python_L1_funciones.py
+++ Python_L1_funciones.py
@@ -28,3 +28,14 @@ def even_bool(num):
 
 evens = filter(even_bool,mylist)
 print(list(evens))
+
+
+
+
+# Lambda Explression
+
+# Filter
+mylist = [1,2,3,4,5,6,7,8]
+
+evens = filter(lambda num:num%2 == 0,mylist)
+print(list(evens))
