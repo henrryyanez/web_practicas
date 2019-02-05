13d8d12a6429f593469bebff6e6c9baaef178d97
hy <henrryyanez@gmail.com>
2019-02-05 11:46:13 -0300

    Return global and local

--- Python_L2_roles.py
+++ Python_L2_roles.py
@@ -23,3 +23,16 @@ def greet():
 
 greet()
 print(name)
+
+#################Llamamos variables gloabales dentro de locales ###################################
+
+x = 50
+
+def func():
+    # global x
+    x = 1000
+    return x
+
+print("before function call, x is: ",x)
+x = func()
+print("after function call, x is: ",x)
